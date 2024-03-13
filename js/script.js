017324e5f746b1a94498af0e6b49d8517bc666d7
scottyTwoskip <scottyeansor@gmail.com>
2024-03-13 11:04:45 -0600

    created displayBackgroundImage function and added it to movie details so far

--- js/script.js
+++ js/script.js
@@ -71,6 +71,9 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIData(`movie/${movieId}`)
 
+    //overlay for background img
+    displayBackgroundImage('movie', movie.backdrop_path)
+
     const div = document.createElement('div')
     div.innerHTML = `<div class="details-top">
           <div>
@@ -117,6 +120,27 @@ async function displayMovieDetails() {
         </div>`
 
     document.querySelector('#movie-details').appendChild(div)
+}
+//display backdrop image function on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div')
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.2';
+    //check for type
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv)
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv)
+    }
 }
 
 //Fetch data from TMDB API
